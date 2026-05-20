31838e6ac44c5354a3d92570bb53eebe8d443014
Ismail <yotech42@gmail.com>
2026-05-20 12:19:16 +0600

    Initial Commit

---
+++ mac-cleaner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mac-cleaner
+//
+//  Created by 6space7 on 20/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mac-cleaner/mac_cleanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  mac_cleanerApp.swift
+//  mac-cleaner
+//
+//  Created by 6space7 on 20/5/26.
+//
+
+import SwiftUI
+
+@main
+struct mac_cleanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
